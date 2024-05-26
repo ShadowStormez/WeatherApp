c37d287483ef14825e1c5739b3579b63cae42bc8
ShadowStormez <ariatn8204@gmail.com>
2024-05-26 21:49:27 +0330

    unsplash API add and style change on search

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('searchButton').addEventListener('click', function() {
     const city = document.getElementById('city').value;
-    const apiKey = '53aa36d40a99579415b1b0b91a3af598'; //API az openWeathermap gereftam
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    fetch(apiUrl)
+    const weatherApiKey = '53aa36d40a99579415b1b0b91a3af598'; //API az openweathermap gereftam
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`;
+    const unsplashApiKey = 'sZMirKM-eeuRTU9lpyQevYUcDeQrwQ3FTH1gJXBuLso'; //API unsplash
+    const unsplashApiUrl = `https://api.unsplash.com/search/photos?query=${city}&client_id=${unsplashApiKey}`;
+    
+    fetch(weatherApiUrl)
         .then(response => response.json())
         .then(data => {
             if (data.cod === '404') {
@@ -26,6 +28,21 @@ document.getElementById('searchButton').addEventListener('click', function() {
             document.getElementById('icon').src = icon;
             document.getElementById('icon').style.display = 'block';
             document.getElementById('icon').alt = description;
+
+            fetch(unsplashApiUrl)
+                .then(response => response.json())
+                .then(imageData => {
+                    if (imageData.results && imageData.results.length > 0) {
+                        const imageUrl = imageData.results[0].urls.regular;
+                        document.body.style.backgroundImage = `url(${imageUrl})`;
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching image:', error);
+                });
+
+                //user search kard yekhurde shafaf mishe forme
+            document.querySelector('.search').style.opacity = '0.5';
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
